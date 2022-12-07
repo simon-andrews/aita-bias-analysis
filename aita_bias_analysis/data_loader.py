0fa11f21229a3ad98ceaf87e6883311ad67257f7
Simon Andrews <public@simonandre.ws>
2022-12-07 04:23:54 -0500

    Preprocessing data, gradient-boosted regressor experiments

--- aita_bias_analysis/data_loader.py
+++ aita_bias_analysis/data_loader.py
@@ -8,7 +8,7 @@ import pandas as pd
 import sklearn
 import textstat
 from demographic_inference import get_age_and_gender
-from judgements import extractJudgment
+from judgements import ah_judgement_ratio, extractJudgment
 from tqdm import tqdm
 from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
 
@@ -34,15 +34,18 @@ def load_post(id: str) -> dict:
     post = json.loads(file_contents)
     post["text"] = clean_text(post["text"])
     post["flesch_reading_ease"] = textstat.flesch_reading_ease(post["text"])
+    post["word_count"] = len(post["text"].split(" "))
     post["op_age"], post["op_gender"] = get_age_and_gender(post["text"])
     post["sentiment"] = SENTIMENT_ANALYZER.polarity_scores(post["text"])["compound"]
     for comment in post["comments"]:
         comment["body"] = clean_text(comment["body"])
+        comment["word_count"] = len(clean_text(comment["body"]).split(" "))
         comment["flesch_reading_ease"] = textstat.flesch_reading_ease(comment["body"])
         comment["judgement"] = extractJudgment(comment["body"])
         comment["sentiment"] = SENTIMENT_ANALYZER.polarity_scores(comment["body"])[
             "compound"
         ]
+    post["ah_judgement_ratio"] = ah_judgement_ratio(post)
     return post
 
 
@@ -62,6 +65,7 @@ def load_posts(start: Optional[int] = None, end: Optional[int] = None) -> List[d
 def flatten_post(post: dict) -> pd.DataFrame:
     records = []
     op_data = {
+        "op_ah_judgement_ratio": post["ah_judgement_ratio"],
         "op_created": post["created"],
         "op_flair": post["flair"],
         "op_flesch_reading_ease": post["flesch_reading_ease"],
@@ -71,6 +75,7 @@ def flatten_post(post: dict) -> pd.DataFrame:
         "op_score": post["score"],
         "op_sentiment": post["sentiment"],
         "op_upvote_ratio": post["upvote_ratio"],
+        "op_word_count": post["word_count"]
     }
     for comment in post["comments"]:
         record = op_data.copy()
@@ -80,6 +85,7 @@ def flatten_post(post: dict) -> pd.DataFrame:
         record["comment_permalink"] = comment["permalink"]
         record["comment_score"] = comment["score"]
         record["comment_sentiment"] = comment["sentiment"]
+        record["comment_word_count"] = comment["word_count"]
         records.append(record)
     df = pd.DataFrame.from_records(records)
     return df
@@ -98,3 +104,4 @@ if __name__ == "__main__":
     posts = load_posts()
     df = flatten_posts(posts)
     print(df.describe())
+    df.to_csv("data.csv")
--- aita_bias_analysis/judgements.py
+++ aita_bias_analysis/judgements.py
@@ -22,6 +22,20 @@ def extractJudgment(comment_body: str) -> Optional[str]:
         return None
 
 
+def ah_judgement_ratio(post: dict) -> float:
+    """
+    What proportion of YTA/ESH/NTA/NAH judgements are YTA or ESH?
+    """
+
+    yta_esh = 0
+    nta_nah = 0
+    for comment in post["comments"]:
+        if comment["judgement"] in {"yta", "esh"}:
+            yta_esh += 1
+        elif comment["judgement"] in {"nta", "nah"}:
+            nta_nah += 1
+    return yta_esh / (yta_esh + nta_nah)
+
 if __name__ == "__main__":
     for post in os.listdir(DATA_DIR):
         with open(DATA_DIR + "/" + post) as json_file:
